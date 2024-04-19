9a97be65721eeb66ac9aa0a23fb4f3777af85e49
basyair7 <basyair.fathul@gmail.com>
2024-04-19 21:37:10 +0700

    add file latihan19, latihan20

---
+++ latihan20.py
@@ -0,0 +1,18 @@
+"""
+cara install library text processing
+linux:
+    python3 -m pip install nama_library
+    
+windows:
+    python -m pip install nama_library
+
+atau:
+    pip install nama_library
+    
+cek version: pip --version
+
+"""
+
+def test1():
+    tesString = "dicoding"
+    tesString.upper()
