12663ed9cbd3326516feb0104e33ca9634c1d62c
basyair7 <basyair.fathul@gmail.com>
2024-04-19 18:31:39 +0700

    modify latihan14.py

--- latihan14.py
+++ latihan14.py
@@ -5,3 +5,15 @@ Artinya, fungsi yang dibuat dapat ditentukan untuk mengembalikan tipe data integ
 """Prosedur adalah deretan instruksi yang jelas keadaan awal (initial state) dan keadaan akhirnya (final state). 
 Prosedur mirip dengan program secara umum, tetapi memiliki cakupan yang kecil dan terbatas."""
 
+def volume_persegi(panjang, lebar, tinggi):
+    return panjang*lebar*tinggi
+
+
+def test1():
+    p = 4
+    l = 4
+    t = 4
+    v = volume_persegi(p,l,t)
+    print(f"Volume persegi dengan p={p} cm, l={l} cm, dan t={t} cm adalah {v} cm^3")
+    
+test1()
